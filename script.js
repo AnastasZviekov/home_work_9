98b6eecd03c1bbb32ae97a9b7a38df3fc3098ba8
AnastasZviekov <stas.zviekov@gmail.com>
2023-08-17 19:11:38 +0200

    add_variable

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-
-const products = [
-    ['apple',10],
-    ['banana',8],
-    ['mango',20],
-    ['grape',18]
-]
+const products = [['apple', 10], ['banana', 8], ['mango', 20], ['grape', 18]]
 
 function summerValue(value) {
-    return value* 0.8;
+    return value * 0.8;
 }
 
-function winterValue(value){
-    return value*2;
+function winterValue(value) {
+    return value * 2;
 }
 
-function getPrice(products, seasonFunc){
-let res=0;
-let totalPrice=0;
+function getPrice(products, seasonFunc) {
+    let res = 0;
+    let totalPrice = 0;
 
-let copiedPrice=[...products];
-for (let i=0; i<copiedPrice.length; i++) {
-  let currentProduct= copiedPrice[i];
+    let copiedPrice = [...products];
+    for (let i = 0; i < copiedPrice.length; i++) {
+        let currentProduct = copiedPrice[i];
 
-  for(let j=0;j<currentProduct.length;j++) {
-    let currentPrice = currentProduct[j];
-    if(typeof currentPrice===`number`) {
-        if (typeof seasonFunc===`function`) {
-            res = seasonFunc(currentPrice);
-            totalPrice += res;
+        for (let j = 0; j < currentProduct.length; j++) {
+            let currentPrice = currentProduct[j];
+            if (typeof currentPrice === `number`) {
+                if (typeof seasonFunc === `function`) {
+                    res = seasonFunc(currentPrice);
+                    totalPrice += res;
+                }
+                else {
+                    totalPrice += currentPrice;
+                }
+            }
         }
-        else {
-            totalPrice+=currentPrice;
-        }
-      }
-   }
- }
-return totalPrice;
+    }
+    return totalPrice;
+
 }
 
-console.log(getPrice(products,summerValue));
-console.log(getPrice(products,winterValue));
-console.log(getPrice(products));
+let totalSummerPrice = getPrice(products, summerValue);
+let totalWinterPrice = getPrice(products, winterValue);
+let totalProdPrice = getPrice(products);
 
+console.log(totalSummerPrice);
+console.log(totalWinterPrice);
+console.log(totalProdPrice);
